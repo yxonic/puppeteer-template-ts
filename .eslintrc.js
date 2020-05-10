f90127d2dbd37f49d50170a1f898449a88279853
Yu Yin <yxonic@gmail.com>
2020-05-10 17:29:29 +0800

    add project template

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  root: true,
+  'env': {
+    'es6': true,
+    'node': true
+  },
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parserOptions: {
+    'sourceType': 'module'
+  },
+  rules: {
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'always'
+    ]
+  }
+};
---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import puppeteer from 'puppeteer';
+
+(async (): Promise<void> => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = (await browser.pages())[0];
+  await page.goto('https://www.baidu.com');
+  await page.type('#kw', 'puppeteer');
+  await page.click('.s_btn_wr input');
+  await page.waitForNavigation({ waitUntil: 'networkidle2' });
+  console.log(await page.evaluate(() => window.location.href));
+  await page.screenshot({ path: 'result.png', fullPage: true });
+  await browser.close();
+})();
